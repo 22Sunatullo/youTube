7f794aebb083513d5e0dfa7ad8b9b8dadde41669
Sunattulo <muslimamuminova6969@gmail.com>
2023-01-02 18:55:16 +1200

    new comit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+
+console.log(randomEmail)
+
+const div = document.querySelector('.name');
+const img = document.querySelector('img');
+
+

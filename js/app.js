551ce8ffaade5e44c81e9d68fe30f626a7552e2c
Sunattulo <muslimamuminova6969@gmail.com>
2023-01-03 05:14:09 +1200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,25 @@ const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
 
 console.log(randomEmail)
 
-const div = document.querySelector('.name');
-const img = document.querySelector('img');
+
+
+const div = {
+    "image":  faker.random.arrayElement([
+      faker.image.nature(), 
+      faker.image.city(), 
+      faker.image.food() 
+    ]),
+    "price": faker.random.number({ min: 20, max: 300 }),
+    "beds": faker.random.number({ min: 1, max: 15 }),
+    "type": faker.random.arrayElement([ 
+      "Entire home", 
+      "Private room", 
+      "Shared room" 
+    ])
+  }
+
+let img = document.querySelector('img');
+
+img.src = div;
 
 

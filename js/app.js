20132dc18cbea9f0d799b3916f7c56501a3f610f
Sunattulo <muslimamuminova6969@gmail.com>
2023-01-06 02:25:47 +1200

    svg

--- js/app.js
+++ js/app.js
@@ -1,27 +1,19 @@
+const navMenu = document.querySelector('.c-hamburger');
+const menu = document.querySelector('.menu');
 
-const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+navMenu.addEventListener('click', function(e){
+    
+    e.preventDefault();
+    // this.classList.toggle('is-active');
+    if(this.classList.contains('is-active')){
+        this.classList.remove('is-active');
+        menu.classList.remove('nav-active');
+        document.body.classList.remove('body-active');
+        
+    }else{
+        this.classList.add('is-active');
+        menu.classList.add('nav-active');
+        document.body.classList.add('body-active');
 
-console.log(randomEmail)
-
-
-
-const div = {
-    "image":  faker.random.arrayElement([
-      faker.image.nature(), 
-      faker.image.city(), 
-      faker.image.food() 
-    ]),
-    "price": faker.random.number({ min: 20, max: 300 }),
-    "beds": faker.random.number({ min: 1, max: 15 }),
-    "type": faker.random.arrayElement([ 
-      "Entire home", 
-      "Private room", 
-      "Shared room" 
-    ])
-  }
-
-let img = document.querySelector('img');
-
-img.src = div;
-
-
+    }
+})
